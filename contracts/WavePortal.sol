3987b7c2f5778361a78edd743e8044008d624474
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-07 02:55:37 +0100

    Add Random Winnner Logic

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,8 @@ import "hardhat/console.sol";
 contract WavePortal{
     uint256 totalWaves;
 
+    uint256 private seed;
+
     event NewWave(address indexed from, uint256 timestamp, string message);
 
     struct Wave {
@@ -17,7 +19,9 @@ contract WavePortal{
     Wave[] waves;
 
     constructor() payable {
-        console.log("Hey! Stay longer with the question");
+        console.log("Hey! Stay with the questions much longer");
+
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public{
@@ -26,15 +30,22 @@ contract WavePortal{
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
-        emit NewWave(msg.sender, block.timestamp, _message);
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %d", seed);
 
-        uint256 prizeAmount = 0.0001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has."
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+            
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
@@ -42,7 +53,6 @@ contract WavePortal{
     }
 
     function getTotalWaves() public view returns (uint256){
-        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
