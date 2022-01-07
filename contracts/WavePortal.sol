a0f244a4d1690297b438bc6136c612aacb869250
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-07 04:08:08 +0100

    Modify Project Files

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -27,8 +27,8 @@ contract WavePortal{
 
     function wave(string memory _message) public{
         require(
-            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
-            "Wait 15m"
+            lastWavedAt[msg.sender] + 10 seconds < block.timestamp,
+            "Must wait 10 seconds before waving again."
         );
         lastWavedAt[msg.sender] = block.timestamp;
         
